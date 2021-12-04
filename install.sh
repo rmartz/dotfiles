200fe1958c96775cfe285e09aa0b820b46a9bbc7
Reed Martz <reed@reedmartz.com>
2021-12-04 11:17:42 -0500

    Add gitignore to install.sh

--- install.sh
+++ install.sh
@@ -1,4 +1,6 @@
 ln ./gitconfig ~/.gitconfig
-ln ./vscode.settings.json ~/Library/Application\ Support/Code//User/settings.json
+ln ./vscode.settings.json ~/Library/Application\ Support/Code/User/settings.json
 ln ./bash_profile ~/.bash_profile
 ln ./vmrc ~/.vimrc
+ln ./gitignore ~/.gitignore
+git config --global core.excludesfile ~/.gitignore
